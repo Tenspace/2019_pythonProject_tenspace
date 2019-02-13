33e6ac0acfc67262927e9b4d2e3456ec945d8ec9
Tenspace <develop@tenspace.co.kr>
2019-02-13 12:33:38 +0900

    upper folder 1st commit

---
+++ 201901_python36/crawlerBot_pack_SCI_2019/crawlerbots/crawler_starter.py
@@ -0,0 +1,8 @@
+from crawlerBot_pack_SCI_2019.crawlerbots.kakaostoryCrawlerBot import kakao_story_crawler_start
+from crawlerBot_pack_SCI_2019.crawlerbots.facebookCrawlerBot import facebook_crawler_start
+from crawlerBot_pack_SCI_2019.crawlerbots.instagramCrawlerBot import instagram_crawler_start
+
+
+facebook_crawler_start()
+kakao_story_crawler_start()
+instagram_crawler_start()
