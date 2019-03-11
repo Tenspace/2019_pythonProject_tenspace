87a579f9ff97e0f836e1b12049def1832c9e3120
HanSangYoon <microscope83@gmail.com>
2019-03-11 14:54:38 +0900

    20190311_01

--- 201901_python36/automation_for_data/generateHashData.py
+++ 201901_python36/automation_for_data/generateHashData.py
@@ -2,13 +2,41 @@ import hashlib
 import random
 import csv
 import requests
+from datetime import datetime, timedelta
+import datetime
+import logging.handlers
+
+# logger 인스턴스를 생성 및 로그 레벨 설정
+logger = logging.getLogger('SerialTime')
+logger.setLevel(logging.DEBUG)
+
+# formatter 생성
+formatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+
+# fileHandler 와 StreamHandler 를 생성
+file_max_bytes = 10 * 1024 * 1024  # log file size : 10MB
+
+fileHandler = logging.handlers.RotatingFileHandler(
+    r"C:\dev_tenspace\2019_python_project_syhan\201901_python36\automation_for_data\log_data\generateTime_log", maxBytes=file_max_bytes, backupCount=10)
+
+streamHandler = logging.StreamHandler()
+
+# handler 에 formatter 세팅
+fileHandler.setFormatter(formatter)
+streamHandler.setFormatter(formatter)
+
+# Handler 를 logging 에 추가
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+
 
 def generateInserQuery(listValues):
 
     insertValueList = []
     loopCnt = 0
-    while(loopCnt != 17679):
-        values = "" + numberGenerate(), platformGen(), random.choice(listValues), random.choice(listValues), \
+    returnTimeStamp = generateTimeSerial()
+    while(loopCnt != 17670):
+        values = "" + returnTimeStamp[loopCnt], numberGenerate(), platformGen(), random.choice(listValues), random.choice(listValues), \
                 random.choice(listValues), random.choice(listValues), numberGenerate(), random.choice(listValues), random.choice(listValues), \
                 random.choice(listValues), random.choice(listValues), random.choice(listValues), random.choice(listValues), \
                 random.choice(listValues), random.choice(listValues),\
@@ -43,6 +71,23 @@ def generateInserQuery(listValues):
 #
 #     writeFile.close()
 #
+
+
+def generateTimeSerial():
+    dt = datetime.datetime(2019, 2, 11)
+    end = datetime.datetime(2019, 4, 12, 4, 34, 23)
+    step = datetime.timedelta(seconds=118)
+
+    result = []
+
+    while dt < end:
+        result.append(dt.strftime('%Y-%m-%d %H:%M:%S'))
+        dt += step
+
+    logger.debug(result)
+    return result
+
+
 
 def platformGen():
     platformList = ['facebook', 'kakaostory', 'instagram']
@@ -102,7 +147,7 @@ if __name__ == '__main__':
     #2. 쿼리 생성 실행
     #print(generateInserQuery(hashValList))
     insert_command = """INSERT INTO crawler_score (
-                 origin_ph, platform, page_id, username, gender, phone_number, birthday, address1, address2, address3,
+                 insertedTime, origin_ph, platform, page_id, username, gender, phone_number, birthday, address1, address2, address3,
                  company1, company2, company3, university1, university2, university3, contact1, contact2,
                  expression_negative, expression_positive, friends_all, friends_residence,
                  friends_company, friends_univ, friends_highschool, friends_native, take_news,
@@ -117,7 +162,7 @@ if __name__ == '__main__':
 
     print(insert_command)
 
-    with open('aster_10.sql', 'w') as out:
+    with open('aster_20190306.sql', 'w') as out:
         out.write(insert_command)
 
 
--- 201901_python36/automation_for_data/testDateSubtraction.py
+++ 201901_python36/automation_for_data/testDateSubtraction.py
@@ -1,5 +1,51 @@
 from datetime import datetime, timedelta
+import datetime
+import logging.handlers
 
-date = "2019-02-12 10:04:34"
+import logging.handlers
 
-thisTime = datetime.strptime(date, "%Y-%m-%d %H:%")
+# logger 인스턴스를 생성 및 로그 레벨 설정
+logger = logging.getLogger('SerialTime')
+logger.setLevel(logging.DEBUG)
+
+# formatter 생성
+formatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+
+# fileHandler 와 StreamHandler 를 생성
+file_max_bytes = 10 * 1024 * 1024  # log file size : 10MB
+
+fileHandler = logging.handlers.RotatingFileHandler(
+    r"C:\dev_tenspace\2019_python_project_syhan\201901_python36\automation_for_data\log_data\generateTime_log", maxBytes=file_max_bytes, backupCount=10)
+
+streamHandler = logging.StreamHandler()
+
+# handler 에 formatter 세팅
+fileHandler.setFormatter(formatter)
+streamHandler.setFormatter(formatter)
+
+# Handler 를 logging 에 추가
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+
+
+
+def generateTimeSerial():
+    dt = datetime.datetime(2019, 2, 11, 10, 4, 44)
+    end = datetime.datetime(2019, 4, 12, 4, 34, 23)
+    step = datetime.timedelta(seconds=67)
+
+    result = []
+
+    while dt < end:
+        result.append(dt.strftime('%Y-%m-%d %H:%M:%S'))
+        dt += step
+
+    print(result)
+    logger.debug(result)
+    logger.debug("================================================")
+    i = 0
+    while i <= len(result)-1:
+        logger.debug(result[i])
+        i += 1
+
+generateTimeSerial()
